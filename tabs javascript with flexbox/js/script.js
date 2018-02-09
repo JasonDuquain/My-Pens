ab2cfc84cdf1121a605603578acfaa7cf5281789
jasonduquain <jduquain@gmail.com>
2018-02-09 17:57:51 -0500

    styles complete

--- tabs javascript with flexbox/js/script.js
+++ tabs javascript with flexbox/js/script.js
@@ -1,23 +1,26 @@
 
 
-let ul = document.querySelector('.tab__list');
-let links = document.querySelectorAll('.tab__link');
-let divs = document.querySelectorAll('.tab__box');
+let ul = document.querySelector('.tab-list__list');
+let links = document.querySelectorAll('.tab-list__link');
+let tabs = document.querySelectorAll('.tab__box');
 
 ul.addEventListener('click', (e) => {
     //only apply js if tab isn't already the active one
-    if (e.target.classList.contains('tab__link') && !e.target.classList.contains('tab__link--is-active')) {
+    if (e.target.classList.contains('tab-list__link') && !e.target.classList.contains('tab-list__link--is-active')) {
         e.preventDefault();
         //loop through all links and divs and remove the active/visible classes
         links.forEach((el, idx) => {
-            el.classList.remove('tab__link--is-active');
-            divs[idx].classList.remove('tab__box--is-visible');
+            el.classList.remove('tab-list__link--is-active');
+            tabs[idx].classList.remove('tab__box--is-visible');
             //add the active class to the clicked link
-            e.target.classList.add('tab__link--is-active');
+            e.target.classList.add('tab-list__link--is-active');
             //if the active link's href attribute matches the div's id attribute (slicing out the #) apply the visible class to that div
-            if (e.target.getAttribute('href').slice(1) === divs[idx].getAttribute('id')) {
-                divs[idx].classList.add('tab__box--is-visible');
+            if (e.target.getAttribute('href').slice(1) === tabs[idx].getAttribute('id')) {
+                tabs[idx].classList.add('tab__box--is-visible');
             }
         });
     }
 });
+
+
+
