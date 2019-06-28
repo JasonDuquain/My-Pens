77c68f0e97085a80c2a6a0025e7075428523a2db
Jason Duquain <jason.duquain@gmail.com>
2019-06-28 15:19:53 -0400

    updated gsap still cannot animate backgroundClip text items

--- Grid Magazine Layouts/Grid Magazine layout 5/js/script.js
+++ Grid Magazine Layouts/Grid Magazine layout 5/js/script.js
@@ -1,12 +1,20 @@
 
 let skiHeading = document.querySelector('.ski__heading');
 
+let fireLetters = Array.from(document.querySelectorAll('.ski__element--fire > div'));
+let earthLetters = Array.from(document.querySelectorAll('.ski__element--earth > div'));
+let waterLetters = Array.from(document.querySelectorAll('.ski__element--water > div'));
+let airLetters = Array.from(document.querySelectorAll('.ski__element--air > div'));
+
 let elementsArray = Array.from(document.querySelectorAll('.ski__element div'));
 
-console.log(elementsArray)
 
 let tl = new TimelineMax();
 let splitHeading = new SplitText(skiHeading);
+
+// add these as high as possible to avoid the content flash solid for a second before animation starts
+//tl.set(['.svg-fire', '.svg-earth', '.svg-water', '.svg-air'], { opacity: 0, scale: 0 });
+//tl.set(['.fire--one', '.fire--two', '.fire--three', '.fire--four'], { opacity: 0 })
 
 function random(min, max) {
     return (Math.random() * (max - min) + min);
@@ -15,27 +23,50 @@ function random(min, max) {
 function hay() {
     
     splitHeading.chars.forEach((el, idx) => {
-        TweenMax.from(el, 2.5, {
-           opacity: 0,
+        TweenMax.from(el, 3, {
+            opacity: 0,
             x: random(-500, 500),
             y: random(-500, 500),
             z: random(-500, 500),
-            scale: 8.1,
-            delay: idx * .02
-        })
+            scale: 8,
+            delay: idx * .02,
+			ease: Power1.easeOut,
+			onComplete: remaining
+        }) 
+		
     })
-    
 }
 
 
-tl.add(hay) // how to get the next two lines to run after this??
-tl.addLabel('now') // nice try
-tl.to('.ski__element--fire', 1, {
-    opacity: 1
-}, "now+=0")
-tl.to('.ski__element--earth', 1, {
-    opacity: 1
-}, "now+=1")
+tl.set(skiHeading, {opacity: 1})
+tl.add(hay) 
+function remaining() {
+	tl.staggerTo(['.svg-fire', '.svg-earth', '.svg-water', '.svg-air'], 1.5, {
+		opacity: 1,
+		scale: 1,
+		ease: Power3.easeOut,
+	}, .3)
+	tl.staggerTo(fireLetters, .5, {
+		opacity: 1,
+		y: 0
+	}, .1, "-=1") 
+	tl.staggerTo(earthLetters, .5, {
+		opacity: 1,
+		y: 0
+	}, .1, "-=.5") 
+	tl.staggerTo(waterLetters, .5, {
+		opacity: 1,
+		y: 0
+	}, .1, "-=.5") 
+	tl.staggerTo(airLetters, .5, {
+		opacity: 1,
+		y: 0
+	}, .1, "-=.5") 
+
+	
+	splitHeading.revert();
+}
+
 
 
 
