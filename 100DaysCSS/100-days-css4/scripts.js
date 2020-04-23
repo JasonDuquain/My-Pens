9c1db9ce0ae9904b3da00a32703e3424f55533d8
Jason Duquain <jduquain@gmail.com>
2020-04-23 10:55:32 -0400

    day 4 pen complete

--- 100DaysCSS/100-days-css4/scripts.js
+++ 100DaysCSS/100-days-css4/scripts.js
@@ -1,18 +1,32 @@
 let circles = document.querySelectorAll('.chart__line--one circle');
-let nums = document.querySelectorAll('.num')
+let nums = document.querySelectorAll('.num');
+let circlesTwo = document.querySelectorAll('.chart__line--two circle');
+let numsTwo = document.querySelectorAll('.numm');
 
-console.log(circles, nums)
 
 circles.forEach(function(el, idx) {
   el.addEventListener('mouseover', function(e) {
     nums[idx].classList.add('num--visible')
-  })
-})
+  });
+});
 
 circles.forEach(function(el, idx) {
   el.addEventListener('mouseout', function(e) {
     nums[idx].classList.remove('num--visible')
-  })
-})
+  });
+});
+
+circlesTwo.forEach(function(el, idx) {
+  el.addEventListener('mouseover', function(e) {
+    numsTwo[idx].classList.add('numm--visible')
+  });
+});
+
+circlesTwo.forEach(function(el, idx) {
+  el.addEventListener('mouseout', function(e) {
+    numsTwo[idx].classList.remove('numm--visible')
+  });
+});
+
 
 
