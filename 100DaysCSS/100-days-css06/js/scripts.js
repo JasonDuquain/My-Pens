ca2677597bd814adbf556e016f0b147c65a51b65
Jason Duquain <jduquain@gmail.com>
2020-04-24 16:54:16 -0400

    day 6 pen complete

--- 100DaysCSS/100-days-css06/js/scripts.js
+++ 100DaysCSS/100-days-css06/js/scripts.js
@@ -1,8 +1,31 @@
 
-let magnifyingGlass = document.querySelector('.notif__magnify');
-let searchBox = document.querySelector('.notif__search');
+const magnifyingGlass = document.querySelector('.notif__magnify');
+const searchBox = document.querySelector('.notif__search');
+
+const svgHamburger = document.querySelector('.notif__svg');
+const mainCard = document.querySelector('.notif');
+const slideOutMenu = document.querySelector('.notif__slideout');
+
+const items = document.querySelectorAll('.notif__item');
+
+
+svgHamburger.addEventListener('click', (e) => {
+  mainCard.classList.toggle('move');
+  slideOutMenu.classList.toggle('move');
+})
 
 magnifyingGlass.addEventListener('click', (e) => {
   searchBox.classList.toggle('visible')
+});
+
+window.addEventListener('load', (e) => {
+  items.forEach((el, idx) => el.classList.add('visible'))
 })
+
+
+
+
+
+
+
 
