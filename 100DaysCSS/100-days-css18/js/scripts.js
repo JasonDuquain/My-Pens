54b5f5f209fbb1966b903c2c87900a176614a89e
Jason Duquain <jduquain@gmail.com>
2020-05-01 05:43:26 -0400

    day 18 pen complete

---
+++ 100DaysCSS/100-days-css18/js/scripts.js
@@ -0,0 +1,13 @@
+
+
+/*let radioButtons = document.querySelectorAll('.radio__btn')
+
+
+
+radioButtons.forEach((el, idx) => {
+  el.addEventListener('click', (e) => {
+    if (el.checked) {
+      console.log('yes')
+    }
+  })
+})*/
